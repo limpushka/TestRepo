295be4cec5576b3a585de81c2e02efa02d5b2626
Denis Chernyakov <d.chernyakov@ALS.local>
2016-09-29 07:02:28 +0300

    Change second statement and add Potato

--- Python.py
+++ Python.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 print "Pizza is the best thing i've ever eat"
-print "Test something for the rebase command!"
+print "Potato is one of my favorite food"
 print "Another statement for rebase"
 print "I dont want it.Please do something else"
